e5f87cdb11d786a47587111b236e8d5b7d8c0374
MukulDevPatel <patelmukuldev@gmail.com>
2023-04-06 20:19:02 +0530

    [Mukul Dev] Add: Employee wage compute for a month

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,30 +8,35 @@ namespace EmployeeWage
 {
     public class EmployeeWage
     {
-        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20, TOTAL_WORKING_DAYS = 20;
         public void Mesurement()
         {
 
             //Varibles
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+
             //Compute
-            int empCheck = random.Next(0,3);
-            switch (empCheck)
+            for (int day = 0; day < TOTAL_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage: " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Total Emp wage: " + totalEmpWage);
         }
     }
 }
